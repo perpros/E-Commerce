65c0abaa7606c843a7c79f2b1b6379fbe7c642a4
MajidF <majidfathi69@gmail.com>
2024-01-07 15:40:05 +0330

    add SizeWidget, SelectedSizeWidget, SelectedColorWidget

---
+++ lib/presention/widgets/selected_color_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SelectedColorWidget extends StatelessWidget{
+  const SelectedColorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/presention/widgets/selected_size_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SelectedSizeWidget extends StatelessWidget{
+  const SelectedSizeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
