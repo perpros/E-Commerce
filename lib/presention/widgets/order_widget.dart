15ef516533a7578a24baadfa538378a1ec584554
Fateme Arjomandi <farjomandy76@gmail.com>
2024-01-13 16:34:30 +0330

    build  order widget

--- lib/presention/widgets/order_widget.dart
+++ lib/presention/widgets/order_widget.dart
@@ -1,13 +1,55 @@
+import 'package:e_commerce/presention/widgets/size_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'color_widget.dart';
 
-class OrderWidget extends StatelessWidget{
+class OrderWidget extends StatelessWidget {
   const OrderWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Row(
+      children: [
+        Image.asset('assets/image/attractive.png'),
+        const SizedBox(width: 15),
+        Expanded(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Jacket Jeans',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                Text(
+                  '\$37.9',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ColorWidget(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    const SizedBox(width: 15),
+                    const SizeWidget('S')
+                  ],
+                )
+              ],
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Icon(
+                Icons.delete_outline,
+                color: Theme.of(context).colorScheme.primary,
+                size: 36,
+              ),
+            )
+          ],
+        ))
+      ],
+    );
   }
 }
