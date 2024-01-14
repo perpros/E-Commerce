4516147c4bbba39b8b2eea11b19bf31ed455a311
Fateme Arjomandi <farjomandy76@gmail.com>
2024-01-14 12:42:37 +0330

    build favorite widget

--- lib/presention/widgets/favorite_widget.dart
+++ lib/presention/widgets/favorite_widget.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
-class FavoriteWidget extends StatelessWidget{
+class FavoriteWidget extends StatelessWidget {
   const FavoriteWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return InkWell(
+      onTap: () {},
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        height: 34,
+        width: 34,
+        decoration: BoxDecoration(
+            color: Colors.white,
+             borderRadius: BorderRadius.circular(34)),
+        child: Icon(
+          Icons.favorite,
+          size: 20,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+      ),
+    );
   }
 }
