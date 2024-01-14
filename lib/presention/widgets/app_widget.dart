c465296717b57a8677dc943bd576cfa25ed2f2e5
Fateme Arjomandi <farjomandy76@gmail.com>
2024-01-14 14:36:22 +0330

    build app widget

--- lib/presention/widgets/app_widget.dart
+++ lib/presention/widgets/app_widget.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
-class AppWidget extends StatelessWidget{
+class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: 44,
+        width: 44,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(44),
+          color: Colors.white,
+        ),
+        child: Icon(
+          Icons.apps_rounded,
+          size: 28,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+      ),
+    );
   }
-
 }
