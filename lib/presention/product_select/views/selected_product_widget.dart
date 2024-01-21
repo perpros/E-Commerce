4333ab665c40b1095e649e390d421f0680338dd8
Fateme Arjomandi <farjomandy76@gmail.com>
2024-01-21 16:03:35 +0330

    02_Product-Select-Screen/widget-layout

---
+++ lib/presention/product_select/views/selected_product_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelectedProductWidget extends StatelessWidget {
+  const SelectedProductWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
