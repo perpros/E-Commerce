9f0fcbab9672438db81a887a43e09fade4e0a54a
Fateme Arjomandi <farjomandy76@gmail.com>
2024-02-04 11:09:14 +0330

    change relative address widgets

--- lib/presention/widgets/appbar_widget.dart
+++ lib/presention/widgets/appbar_widget.dart
@@ -1,7 +1,7 @@
-import 'package:e_commerce/presention/widgets/avatar_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'app_widget.dart';
+import 'avatar_widget.dart';
 
 class AppbarWidget extends StatelessWidget {
   const AppbarWidget({super.key});
