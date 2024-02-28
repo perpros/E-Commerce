a1e9e8492c4a7bb1c35168f92b1f2214f3d9830d
Fateme Arjomandi <farjomandy76@gmail.com>
2024-02-28 13:45:25 +0330

    get services build provider

---
+++ lib/data/provider/dio_provider.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+import 'package:dio/dio.dart';
+
+class DioProvider {
+  Future<dynamic> getMethod(String url) async {
+    Dio dio = Dio();
+    dio.options.headers['content-type'] = 'application/json ';
+    return await dio
+        .get(url,
+            options: Options(responseType: ResponseType.json, method: 'Get'))
+        .then((response) {
+      log(response.toString() as num);
+      return response;
+    });
+  }
+}
